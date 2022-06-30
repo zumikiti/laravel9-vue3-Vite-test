b36e66a53178548d3a7183d0984b68f94b57e3d5
akihiro higashiizumi <akihiro@akihironoMac-mini.local>
2022-07-01 06:04:52 +0900

    [add] コンテナ内で npm run dev できるように設定

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
-            },
-        }),
-    ],
+            }),
+        ],
+        server: {
+            host: '0.0.0.0',
+            port: env.VITE_PORT,
+        },
+    }
 });
